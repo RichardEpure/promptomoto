0d81412de3bd52428fa0e8578be2fa3ad33d4678
Richard <43828146+RichardEpure@users.noreply.github.com>
2026-02-11 21:05:49 +0000

    tweak styles

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -33,7 +33,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" suppressHydrationWarning>
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} h-full bg-zinc-50 antialiased dark:bg-black`}
+            >
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="dark"
