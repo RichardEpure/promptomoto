a4ee2bce6bb69acf20400a9b923feba5d47dec2f
Richard <43828146+RichardEpure@users.noreply.github.com>
2026-02-14 16:14:39 +0000

    remove cors middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,6 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 
 from app.database import create_db_and_tables
@@ -15,18 +14,6 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-
-origins = [
-    "http://localhost:3000",
-]
-
-app.add_middleware(
-    CORSMiddleware,  # type: ignore
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 app.include_router(auth.router)
 app.include_router(users.router)
